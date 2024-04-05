075a99593b5beb063807dc9fe7a145113d4c9c60
ST10454062 <st10454062@vcconnect.edu.za>
2024-04-05 09:52:09 +0200

    Initial commit

---
+++ ST10454062IMADProject1/app/src/main/java/com/example/st10454062/st10454062imadproject1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.st10454062.st10454062imadproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
